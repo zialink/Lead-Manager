e933f789d67750466f9f1e3ffd8750d2bd3881d6
zialink <zedlinkus@gmail.com>
2019-11-28 01:19:31 +0100

    new update to lead manager app

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,10 +1,25 @@
-import React, { Component } from 'react';
-import ReactDom from 'react-dom';
+import React, { Component, Fragment } from "react";
+import ReactDOM from "react-dom";
+
+import Header from "./layout/Header";
+import Dashboard from "./leads/dashboard";
+
+import { Provider } from "react-redux";
+import store from "../store";
 
 class App extends Component {
-    render() {
-        return <h1>React App</h1>
-    }
+  render() {
+    return (
+      <Provider store={store}>
+        <Fragment>
+          <Header />
+          <div className="container">
+            <Dashboard />
+          </div>
+        </Fragment>
+      </Provider>
+    );
+  }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
